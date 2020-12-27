78ee517d36e864760745fe45bc9f7c6560baaff4
mkellsy <mkellsy@gmail.com>
2020-12-27 10:39:50 -0700

    use inset for overlays

--- src/components/elements/modal.vue
+++ src/components/elements/modal.vue
@@ -63,10 +63,7 @@
 <style lang="scss">
     #dialog {
         position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
+        inset: 0;
         display: flex;
         align-items: center;
         box-sizing: border-box;
