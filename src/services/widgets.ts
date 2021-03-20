a1305a82bab97a85982aa159c218f88232fdeb71
mkellsy <mkellsy@gmail.com>
2021-03-19 22:15:39 -0600

    add cpu temp widget

--- src/services/widgets.ts
+++ src/services/widgets.ts
@@ -21,12 +21,20 @@ export const available = [{
     label: "activity",
     selected: false,
 }, {
-    name: "current-widget",
-    label: "weather",
+    name: "system-widget",
+    label: "system_info",
     selected: false,
 }, {
     name: "cpu-widget",
     label: "cpu",
+    selected: false,
+}, {
+    name: "current-widget",
+    label: "weather",
+    selected: false,
+}, {
+    name: "temperature-widget",
+    label: "cpu_temperature",
     selected: false,
 }, {
     name: "forecast-widget",
@@ -47,10 +55,6 @@ export const available = [{
 }, {
     name: "log-widget",
     label: "log",
-    selected: false,
-}, {
-    name: "system-widget",
-    label: "system_info",
     selected: false,
 }];
 
@@ -82,17 +86,22 @@ export function layout(name: string): { [key: string]: any } | undefined {
 
         case "cpu-widget":
             return {
-                x: 0, y: 0, w: 7, h: 18, i: "cpu-widget", component: "cpu-widget",
+                x: 0, y: 0, w: 6, h: 8, i: "cpu-widget", component: "cpu-widget",
+            };
+
+        case "temperature-widget":
+            return {
+                x: 0, y: 0, w: 6, h: 8, i: "temperature-widget", component: "temperature-widget",
             };
 
         case "memory-widget":
             return {
-                x: 0, y: 0, w: 7, h: 18, i: "memory-widget", component: "memory-widget",
+                x: 0, y: 0, w: 6, h: 8, i: "memory-widget", component: "memory-widget",
             };
 
         case "bridges-widget":
             return {
-                x: 0, y: 0, w: 7, h: 18, i: "bridges-widget", component: "bridges-widget",
+                x: 0, y: 0, w: 6, h: 8, i: "bridges-widget", component: "bridges-widget",
             };
 
         case "log-widget":
@@ -130,6 +139,7 @@ export function widgets(): { [key: string]: () => any } {
         "accessory-widget": () => import(/* webpackChunkName: "widget-accessory" */ "@/components/widgets/accessory.vue"),
         "activity-widget": () => import(/* webpackChunkName: "widget-activity" */ "@/components/widgets/activity.vue"),
         "cpu-widget": () => import(/* webpackChunkName: "widget-cpu" */ "@/components/widgets/cpu.vue"),
+        "temperature-widget": () => import(/* webpackChunkName: "widget-temperature" */ "@/components/widgets/temperature.vue"),
         "memory-widget": () => import(/* webpackChunkName: "widget-memory" */ "@/components/widgets/memory.vue"),
         "bridges-widget": () => import(/* webpackChunkName: "widget-bridges" */ "@/components/widgets/bridges.vue"),
         "system-widget": () => import(/* webpackChunkName: "widget-system" */ "@/components/widgets/system.vue"),
