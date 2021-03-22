1f4ae20203051772a84261c960bc60e889e9bd76
mkellsy <mkellsy@gmail.com>
2021-03-22 11:44:30 -0600

    some formatting

--- src/views/setup.vue
+++ src/views/setup.vue
@@ -15,6 +15,7 @@
  | You should have received a copy of the GNU General Public License                              |
  | along with this program.  If not, see <http://www.gnu.org/licenses/>.                          |
  -------------------------------------------------------------------------------------------------->
+
 <template>
     <div :key="version" id="setup">
         <modal v-if="loading" :welcome="$t('welcome')" width="760px" height="670px">
